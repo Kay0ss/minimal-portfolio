8608ed090e43d1e7c9a50fb2217f482fb9e4fb3c
Jack <85977030+Kay0ss@users.noreply.github.com>
2022-01-08 14:08:14 -0600

    got 'toggle lockdown' button working inside intro card

--- src/Cards/intro.js
+++ src/Cards/intro.js
@@ -1,12 +1,129 @@
 import React from "react";
 import "./cards.css";
+// import noLockdown from "../images/no-lockdown.png";
+// import lockdown from "../images/lockdown.png";
+import ReactDOM from "react-dom";
 
-function Intro() {
-    return(
-        <div className="box intro">
-            <p>I'm <span className="jack">jack</span>, I'm a full stack web developer from Minneapolis. I'm interested in React, Node, Design, and building beautiful web-apps.</p>
+class Intro extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const lockdown = require('../images/lockdown.png');
+        const nolockdown = require('../images/no-lockdown.png');
+
+        this.switchImage = this.switchImage.bind(this);
+        this.state = {
+          currentImage: 0,
+          images: [
+            lockdown, nolockdown
+          ]
+        };
+      }
+      switchImage() {
+        if (this.state.currentImage < this.state.images.length - 1) {
+          this.setState({
+            currentImage: this.state.currentImage + 1
+          });
+        } else {
+          this.setState({
+            currentImage: 0
+          });
+        }
+        return this.currentImage;
+      }
+
+    //   componentDidMount() {
+    //     setInterval(this.switchImage, 1000);
+    //   }
+
+  render() {
+    return (
+      <div className="box intro">
+        <div className="intro-content">
+          <img className="profile-pic" src={this.state.images[this.state.currentImage]} alt="self portrait of me" />
+          <p className="intro-text">
+            I'm <span className="jack">jack</span>, I'm a full stack web
+            developer from Minneapolis. I'm interested in React, Node, and
+            Design.
+          </p>
+          <button onClick={this.switchImage}>click me</button>
+          <a class="button-toggle"
+            onClick={this.switchImage}
+          >
+              
+            <svg
+              id="Arrow_Rotate.2"
+              data-name="Arrow, Rotate.2"
+              xmlns="http://www.w3.org/2000/svg"
+              width="15.702"
+              height="15.702"
+              viewBox="0 0 15.702 15.702"
+            >
+              <g
+                id="Group_6"
+                data-name="Group 6"
+                transform="translate(2.62 2.617)"
+              >
+                <path
+                  id="Path_5"
+                  data-name="Path 5"
+                  d="M14.8,12.032a5.229,5.229,0,0,1-9.824,2.482"
+                  transform="translate(-4.34 -6.777)"
+                  fill="none"
+                  stroke="#0D1117"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.4"
+                ></path>
+                <path
+                  id="Path_6"
+                  data-name="Path 6"
+                  d="M4,9.149A5.231,5.231,0,0,1,13.83,6.731"
+                  transform="translate(-4.004 -4)"
+                  fill="none"
+                  stroke="#0D1117"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.4"
+                ></path>
+                <path
+                  id="Path_7"
+                  data-name="Path 7"
+                  d="M15.953,6.952h2.313V4.639"
+                  transform="translate(-8.135 -4.221)"
+                  fill="none"
+                  stroke="#0D1117"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.4"
+                ></path>
+                <path
+                  id="Path_8"
+                  data-name="Path 8"
+                  d="M6.825,15.825H4.512v2.313"
+                  transform="translate(-4.18 -8.088)"
+                  fill="none"
+                  stroke="#0D1117"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.4"
+                ></path>
+              </g>
+              <path
+                id="Path_9"
+                data-name="Path 9"
+                d="M0,0H15.7V15.7H0Z"
+                fill="none"
+              ></path>
+            </svg>
+            <p>Toggle Lockdown</p>
+          </a>
         </div>
-    )
+      </div>
+    );
+  }
 }
 
 export default Intro;
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<Intro />, rootElement);
