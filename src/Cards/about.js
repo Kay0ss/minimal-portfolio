ad48a55bd9f0cc1812efd86e71d3916cd655e4e7
Jack <85977030+Kay0ss@users.noreply.github.com>
2022-01-15 19:04:21 -0600

    gh-pages fix?

--- src/Cards/about.js
+++ src/Cards/about.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <div className="box">
       <div className="about-container">
-        <div className="about-content t">
+        <div className="about-content">
           <span className="jack">I believe</span> there are so many things
           in this world that are yet to be done and so many new{" "}
           <span className="jack">stories</span> to be told. My goal in life
