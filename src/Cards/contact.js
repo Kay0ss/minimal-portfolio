c9020be25029925b5dc9a56f74787fe88891f418
Jack <85977030+Kay0ss@users.noreply.github.com>
2022-01-15 22:20:47 -0600

    fixed github not showing up on mobile

--- src/Cards/contact.js
+++ src/Cards/contact.js
@@ -2,9 +2,13 @@ import React from "react";
 import "./cards.css";
 
 function Contact() {
-    return(
-        <div className="box">Contact</div>
-    )
+  return (
+    <div className="box">
+      <div style={{height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <h1 style={{}}>Contact</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
--- src/Cards/project3.js
+++ src/Cards/project3.js
@@ -5,8 +5,8 @@ import proj3 from "../images/project3-bg.png";
 function Project3() {
   return (
     <div className="box">
-      <div>
-        <img className="proj-bg-long" src={proj3} alt="..." />
+      <div className="proj-container">
+        {/* <img className="proj-bg-long" src={proj3} alt="..." /> */}
       </div>
     </div>
   );
