c6bc731ed323c74b93214991510e78939123c0cf
Jack <85977030+Kay0ss@users.noreply.github.com>
2022-01-16 15:54:20 -0600

    navbar styling

--- src/nav.js
+++ src/nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./nav.css";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 // function addClass() {
 //   document.getElementById("about").classList.add("clicked");
@@ -11,18 +11,18 @@ function nav() {
     <div className="nav-container">
       <Link to="/" className="logo">jack</Link>
       <div className="navbar">
-        <Link to="/" id="nav-btn">
+        <NavLink to="/" activeClassName="active" id="nav-btn">
           Home
-        </Link>
-        <Link to="/About" id="nav-btn">
+        </NavLink>
+        <NavLink to="/About" id="nav-btn">
           About
-        </Link>
-        <Link to="/Projects" id="nav-btn">
+        </NavLink>
+        <NavLink to="/Projects" id="nav-btn">
           Projects
-        </Link>
-         <Link to="/Tools" id="nav-btn">
+        </NavLink>
+         <NavLink to="/Tools" id="nav-btn">
           Tools
-        </Link>
+        </NavLink>
       </div>
       <h1 className="nav-contact">contact</h1>
     </div>
