d610b8f0b94442a997a08f09f29c3c314d01cb7d
Jack <85977030+Kay0ss@users.noreply.github.com>
2022-02-14 12:06:40 -0600

    more work on project page

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,5 +1,8 @@
 import react from "react";
-import captionMe from "../../images/photoshop.png";
+// import captionMe from "../../images/photoshop.png";
+import SWQ from "../../images/star-wars-quiz.png";
+import TC from "../../images/True-Crime.png";
+import CM from "../../images/caption-me.png";
 import { BsArrowUpRight } from "react-icons/bs";
 import "./projects.scss";
 
@@ -7,8 +10,8 @@ function Projects() {
   return (
     <>
       <div className="proj-page-container">
-        <article className="proj box">
-          <img src={captionMe} alt="captionMe" className="proj-logo" />
+        <article className="proj box cm">
+          {/* <img src={CM} alt="captionMe" className="proj-logo" /> */}
           {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
           <a
             className="arrow-btn-long"
@@ -19,8 +22,21 @@ function Projects() {
             <p className="btn-text">CaptionMe(me)</p>
           </a>
         </article>
-        <article className="proj box">
-          <img src={captionMe} alt="captionMe" className="proj-logo" />
+        <article className="proj box tc">
+          {/* <img src={TC} alt="captionMe" className="proj-logo" /> */}
+          {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+
+          <a
+            className="arrow-btn-long"
+            href="https:www.jackt.xyz"
+            target="_blank"
+          >
+            <BsArrowUpRight />
+            <p className="btn-text">Old Site</p>
+          </a>
+        </article>
+        <article className="proj box swq">
+          {/* <img src={SWQ} alt="captionMe" className="proj-logo" /> */}
           {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
 
           <a
@@ -33,21 +49,8 @@ function Projects() {
           </a>
         </article>
         <article className="proj box">
-          {/* <img src={captionMe} alt="captionMe" className="proj-logo" /> */}
-          <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" />
-
-          <a
-            className="arrow-btn-long"
-            href="https:www.jackt.xyz"
-            target="_blank"
-          >
-            <BsArrowUpRight />
-            <p className="btn-text">Old Site</p>
-          </a>
-        </article>
-        <article className="proj box">
-          {/* <img src={captionMe} alt="captionMe" className="proj-logo" /> */}
-          <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" />
+          <img src={CM} alt="captionMe" className="proj-logo" />
+          {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
 
           <a
             className="arrow-btn-long"
