2b0aa7fb2cf6da6206eeda385d7fa5a8c7bcfea2
Jack <85977030+Kay0ss@users.noreply.github.com>
2022-02-14 23:15:54 -0600

    more work on projects

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -4,63 +4,116 @@ import SWQ from "../../images/star-wars-quiz.png";
 import TC from "../../images/True-Crime.png";
 import CM from "../../images/caption-me.png";
 import { BsArrowUpRight } from "react-icons/bs";
+import { RiGithubLine } from "react-icons/ri";
 import "./projects.scss";
 
 function Projects() {
   return (
     <>
       <div className="proj-page-container">
-        <article className="proj box cm">
-          {/* <img src={CM} alt="captionMe" className="proj-logo" /> */}
-          {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
-          <a
-            className="arrow-btn-long"
-            href="https:www.jackt.xyz"
-            target="_blank"
-          >
-            <BsArrowUpRight />
-            <p className="btn-text">CaptionMe(me)</p>
-          </a>
-        </article>
-        <article className="proj box tc">
-          {/* <img src={TC} alt="captionMe" className="proj-logo" /> */}
-          {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+        <a
+          href="jackt.xyz"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="proj box cm"
+        >
+          <article>
+            {/* <img src={CM} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+            <a
+              className="arrow-btn-long-proj"
+              href="https:www.jackt.xyz"
+              target="_blank"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">CaptionMe(me)</p>
+            </a>
+            <p style={{ opacity: "0" }} className="description">
+              this is just a test, test, test, test, test, test, test, test,
+              test, ============, ======,=,=,=====df=, ds=f,sdf=s=d,f
+            </p>
+            <a
+              className="arrow-btn-git"
+              href="google.com"
+              target="_blank"
+              style={{ left: "71.5%" }}
+            >
+              <RiGithubLine />
+            </a>
+          </article>
+        </a>
+        <a href="jackt.xyz" target="_blank" className="proj box tc">
+          <article>
+            {/* <img src={TC} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
 
-          <a
-            className="arrow-btn-long"
-            href="https:www.jackt.xyz"
-            target="_blank"
-          >
-            <BsArrowUpRight />
-            <p className="btn-text">Old Site</p>
-          </a>
-        </article>
-        <article className="proj box swq">
-          {/* <img src={SWQ} alt="captionMe" className="proj-logo" /> */}
-          {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+            <a
+              className="arrow-btn-long-proj"
+              href="https:www.jackt.xyz"
+              target="_blank"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">True Crime Quiz</p>
+            </a>
+            <p style={{ opacity: "0" }} className="description">
+              this is just a test, test, test, test, test, test, test, test,
+              test, ============, ======,=,=,=====df=, ds=f,sdf=s=d,f
+            </p>
+            <a
+              style={{ left: "73%" }}
+              className="arrow-btn-git"
+              href="google.com"
+              target="_blank"
+            >
+              <RiGithubLine />
+            </a>
+          </article>
+        </a>
+        <a href="jackt.xyz" target="_blank" className="proj box swq">
+          <article>
+            {/* <img src={SWQ} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
 
-          <a
-            className="arrow-btn-long"
-            href="https:www.jackt.xyz"
-            target="_blank"
-          >
-            <BsArrowUpRight />
-            <p className="btn-text">Old Site</p>
-          </a>
-        </article>
-        <article className="proj box">
-          <img src={CM} alt="captionMe" className="proj-logo" />
-          {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+            <a
+              className="arrow-btn-long-proj"
+              href="https:www.jackt.xyz"
+              target="_blank"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">Star Quiz</p>
+            </a>
+            <p style={{ opacity: "0" }} className="description">
+              this is just a test, test, test, test, test, test, test, test,
+              test, ============, ======,=,=,=====df=, ds=f,sdf=s=d,f
+            </p>
+            <a className="arrow-btn-git" href="google.com" target="_blank">
+              <RiGithubLine />
+            </a>
+          </article>
+        </a>
+        <a href="jackt.xyz" target="_blank" className="proj box nft">
+          <article>
+            {/* <img src={CM} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
 
-          <a
-            className="arrow-btn-long"
-            href="https:www.jackt.xyz"
-            target="_blank"
-          >
-            <BsArrowUpRight />
-            <p className="btn-text">Old Site</p>
-          </a>
-        </article>
+            <a
+              className="arrow-btn-long-proj"
+              href="https://kay0ss.github.io/nft-preview-card/"
+              target="_blank"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">Nft Card</p>
+            </a>
+            <p style={{ opacity: "0" }} className="description">
+              this is just a test, test, test, test, test, test, test, test,
+              test, ============, ======,=,=,=====df=, ds=f,sdf=s=d,f
+            </p>
+
+            <a className="arrow-btn-git" href="https://github.com/Kay0ss/nft-preview-card" target="_blank">
+              <RiGithubLine />
+            </a>
+          </article>{" "}
+        </a>
       </div>
     </>
   );
