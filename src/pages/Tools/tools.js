5f7c0b718df83e5ca436c78d59c8dbbcb3f700b7
Jack <85977030+Kay0ss@users.noreply.github.com>
2022-02-15 12:44:49 -0600

    finished tools page

--- src/pages/Tools/tools.js
+++ src/pages/Tools/tools.js
@@ -1,9 +1,298 @@
 import react from "react";
+import { BsArrowUpRight } from "react-icons/bs";
+import "./tools.scss";
 
 function Tools() {
-    return(
-        <h1>tools</h1>
-    )
+  return (
+    <>
+      <h1
+        className="tools"
+        style={{
+          fontSize: "30px",
+          fontWeight: "bold",
+          maxWidth: "1200px",
+          margin: "auto",
+        }}
+      >
+        &nbsp;&nbsp;Tools I use
+      </h1>
+      &nbsp;
+      <div className="proj-page-container">
+        <a
+          href="https://www.adobe.com/products/photoshop/free-trial-download.html?sdid=1NZGDDHD&mv=search&ef_id=Cj0KCQiAu62QBhC7ARIsALXijXSbNvX2RMzua1YdYcBYSmvidBqYR4o3daExPLyIbDndoMb08iTpGd4aAsXHEALw_wcB:G:s&s_kwcid=AL!3085!3!452570596987!e!!g!!photoshop!10687808131!102260248461&gclid=Cj0KCQiAu62QBhC7ARIsALXijXSbNvX2RMzua1YdYcBYSmvidBqYR4o3daExPLyIbDndoMb08iTpGd4aAsXHEALw_wcB"
+          target="_blank"
+          rel="noreferrer"
+          className="proj box Photoshop"
+        >
+          <article>
+            {/* <img src={SWQ} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+
+            <a
+              className="arrow-btn-long-proj"
+              href="https://www.adobe.com/products/photoshop/free-trial-download.html?sdid=1NZGDDHD&mv=search&ef_id=Cj0KCQiAu62QBhC7ARIsALXijXSbNvX2RMzua1YdYcBYSmvidBqYR4o3daExPLyIbDndoMb08iTpGd4aAsXHEALw_wcB:G:s&s_kwcid=AL!3085!3!452570596987!e!!g!!photoshop!10687808131!102260248461&gclid=Cj0KCQiAu62QBhC7ARIsALXijXSbNvX2RMzua1YdYcBYSmvidBqYR4o3daExPLyIbDndoMb08iTpGd4aAsXHEALw_wcB"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">Photoshop</p>
+            </a>
+          </article>
+        </a>
+        <a
+          href="www.figma.com"
+          target="_blank"
+          rel="noreferrer"
+          className="proj box Figma"
+        >
+          <article>
+            {/* <img src={TC} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+
+            <a
+              className="arrow-btn-long-proj"
+              href="www.figma.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">Figma</p>
+            </a>
+          </article>
+        </a>
+        <a
+          href="https://developer.chrome.com/docs/devtools/"
+          target="_blank"
+          rel="noreferrer"
+          rel="noopener noreferrer"
+          className="proj box CDT"
+        >
+          <article>
+            {/* <img src={CM} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+            <a
+              className="arrow-btn-long-proj"
+              href="https://developer.chrome.com/docs/devtools/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">Chrome DevTools</p>
+            </a>
+          </article>
+        </a>
+        <a
+          href="https://en.wikipedia.org/wiki/CSS"
+          target="_blank"
+          rel="noreferrer"
+          className="proj box CSS"
+        >
+          <article>
+            {/* <img src={SWQ} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+
+            <a
+              className="arrow-btn-long-proj"
+              href="https://en.wikipedia.org/wiki/CSS"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">CSS</p>
+            </a>
+          </article>
+        </a>
+        <a
+          href="https://getbootstrap.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="proj box Bootstrap"
+        >
+          <article>
+            {/* <img src={CM} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+
+            <a
+              className="arrow-btn-long-proj"
+              href="https://getbootstrap.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">Bootstrap</p>
+            </a>
+          </article>
+        </a>
+
+        <a
+          href="www.github.com"
+          target="_blank"
+          rel="noreferrer"
+          className="proj box Gith"
+        >
+          <article>
+            {/* <img src={SWQ} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+
+            <a
+              className="arrow-btn-long-proj"
+              href="www.github.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">Github</p>
+            </a>
+          </article>
+        </a>
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"
+          target="_blank"
+          rel="noreferrer"
+          className="proj box HTML"
+        >
+          <article>
+            {/* <img src={SWQ} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+
+            <a
+              className="arrow-btn-long-proj"
+              href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">HTML</p>
+            </a>
+          </article>
+        </a>
+
+        <a
+          href="https://www.javascript.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="proj box Javascript"
+        >
+          <article>
+            {/* <img src={SWQ} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+
+            <a
+              className="arrow-btn-long-proj"
+              href="https://www.javascript.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">JavaScript</p>
+            </a>
+          </article>
+        </a>
+      </div>
+      <h1
+        className="tools"
+        style={{
+          fontSize: "30px",
+          fontWeight: "bold",
+          maxWidth: "1200px",
+          margin: "auto",
+          marginTop: "50px",
+        }}
+      >
+        &nbsp;&nbsp;Tools I'm Learning
+      </h1>
+      &nbsp;
+      <div className="proj-page-container">
+        <a
+          href="https://reactjs.org/"
+          target="_blank"
+          rel="noreferrer"
+          className="proj box React"
+        >
+          <article>
+            {/* <img src={SWQ} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+
+            <a
+              className="arrow-btn-long-proj"
+              href="https://reactjs.org/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">React</p>
+            </a>
+          </article>
+        </a>
+
+        <a
+          href="https://mui.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="proj box Material"
+        >
+          <article>
+            {/* <img src={SWQ} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+
+            <a
+              className="arrow-btn-long-proj"
+              href="https://mui.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">Material Ui</p>
+            </a>
+          </article>
+        </a>
+
+        <a
+          href="https://sass-lang.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="proj box Sass"
+        >
+          <article>
+            {/* <img src={SWQ} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+
+            <a
+              className="arrow-btn-long-proj"
+              href="https://sass-lang.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">Sass</p>
+            </a>
+          </article>
+        </a>
+
+        <a
+          href="https://tailwindcss.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="proj box Tailwind"
+        >
+          <article>
+            {/* <img src={SWQ} alt="captionMe" className="proj-logo" /> */}
+            {/* <iframe className="proj-bg" src="https://kay0ss.github.io/csb-0g3fh/" /> */}
+
+            <a
+              className="arrow-btn-long-proj"
+              href="https://tailwindcss.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsArrowUpRight />
+              <p className="btn-text">Tailwind</p>
+            </a>
+          </article>
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default Tools;
